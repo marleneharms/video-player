00b86bb24f8154403d9a1934376cc88663bf24ab
“marleneharms” <marlene_harms@hotmail.com>
2023-08-24 00:34:22 -0600

    added js variables

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-
+const video = document.querySelector("video"); //element
+const progressRange = document.querySelector(".progress-range"); // class
+const progressBar = document.querySelector(".progress-bar");
+const playBtn = document.getElementById("play-btn");
+const volumeIcon = document.getElementById("volume-icon");
+const volumeRange = document.querySelector(".volume-range");
+const volumeBar = document.querySelector(".volume-bar");
+const currentTime = document.querySelector(".time-elapsed");
+const duration = document.querySelector(".time-duration");
+const fullscreen = document.querySelector(".fullscreen");
 
 // Play & Pause ----------------------------------- //
 
-
-
 // Progress Bar ---------------------------------- //
-
-
 
 // Volume Controls --------------------------- //
 
-
-
 // Change Playback Speed -------------------- //
 
-
-
 // Fullscreen ------------------------------- //
-
-
