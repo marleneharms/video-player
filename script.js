d7602e26b21398fe80563f5e5620ff0c1e4c73ff
“marleneharms” <marlene_harms@hotmail.com>
2023-08-26 01:50:55 -0600

    add progress bar progress and time

--- script.js
+++ script.js
@@ -32,6 +32,28 @@ video.addEventListener("ended", showPlayIcon);
 
 // Progress Bar ---------------------------------- //
 
+// Calculate Display time format
+
+function displayTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = Math.floor(time % 60);
+  seconds = seconds > 9 ? seconds : `0${seconds}`;
+  return `${minutes}:${seconds}`;
+}
+
+// update the progress bar as video plays
+function updateProgress() {
+  // console.log("currentTime", video.currentTime, "duration", video.duration);
+  progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+  currentTime.textContent = `${displayTime(video.currentTime)} /`;
+  duration.textContent = `${displayTime(video.duration)}`;
+}
+
+// Click to seek within the video
+function setProgress(e) {
+  console.log(e);
+}
+
 // Volume Controls --------------------------- //
 
 // Change Playback Speed -------------------- //
@@ -41,3 +63,6 @@ video.addEventListener("ended", showPlayIcon);
 // Event Listeners
 playBtn.addEventListener("click", togglePlay);
 video.addEventListener("click", togglePlay);
+video.addEventListener("timeupdate", updateProgress);
+video.addEventListener("canplay", updateProgress);
+progressRange.addEventListener("click", setProgress);
