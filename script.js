e4375c43c00c936c1ebd0d26440d6c8d6c74a651
“marleneharms” <marlene_harms@hotmail.com>
2023-08-29 01:28:54 -0600

    volume and speed add

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const volumeBar = document.querySelector(".volume-bar");
 const currentTime = document.querySelector(".time-elapsed");
 const duration = document.querySelector(".time-duration");
 const fullscreen = document.querySelector(".fullscreen");
+const speed = document.querySelector(".player-speed");
 
 // Play & Pause ----------------------------------- //
 
@@ -51,13 +52,62 @@ function updateProgress() {
 
 // Click to seek within the video
 function setProgress(e) {
-  console.log(e);
+  const newTime = e.offsetX / progressRange.offsetWidth; //offsetWidth is the total width of the progress bar
+  progressBar.style.width = `${newTime * 100}%`;
+  video.currentTime = newTime * video.duration; // new duration
+  console.log(newTime * 100);
 }
 
 // Volume Controls --------------------------- //
 
+let lastVolume = 1; // 1 is the default volume
+
+// Volume Bar
+function changeVolume(e) {
+  let volume = e.offsetX / volumeRange.offsetWidth;
+  // rounding volume up or down
+  if (volume < 0.1) {
+    volume = 0;
+  }
+  if (volume > 0.9) {
+    volume = 1;
+  }
+  volumeBar.style.width = `${volume * 100}%`;
+  video.volume = volume;
+  // change icon depending on volume
+  volumeIcon.className = "";
+  if (volume > 0.7) {
+    volumeIcon.classList.add("fas", "fa-volume-up");
+  } else if (volume < 0.7 && volume > 0) {
+    volumeIcon.classList.add("fas", "fa-volume-down");
+  } else if (volume === 0) {
+    volumeIcon.classList.add("fas", "fa-volume-off");
+  }
+  lastVolume = volume;
+}
+
+// mute/unmute
+
+function toggleMute() {
+  volumeIcon.className = "";
+  if (video.volume) {
+    lastVolume = video.volume;
+    video.volume = 0;
+    volumeBar.style.width = 0;
+    volumeIcon.classList.add("fas", "fa-volume-mute");
+    volumeIcon.setAttribute("title", "Unmute");
+  } else {
+    video.volume = lastVolume;
+    volumeBar.style.width = `${lastVolume * 100}%`;
+    volumeIcon.classList.add("fas", "fa-volume-up");
+    volumeIcon.setAttribute("title", "Mute");
+  }
+}
 // Change Playback Speed -------------------- //
 
+function changeSpeed() {
+  video.playbackRate = speed.value; // Change playback rate of the video to speed specified
+}
 // Fullscreen ------------------------------- //
 
 // Event Listeners
@@ -66,3 +116,6 @@ video.addEventListener("click", togglePlay);
 video.addEventListener("timeupdate", updateProgress);
 video.addEventListener("canplay", updateProgress);
 progressRange.addEventListener("click", setProgress);
+volumeRange.addEventListener("click", changeVolume);
+volumeIcon.addEventListener("click", toggleMute);
+speed.addEventListener("change", changeSpeed);
